476daa2202af88f22934de0a5c2b8539a24cb289
Callum Macrae <callumacrae95@gmail.com>
2011-03-24 19:18:58 +0000

    Refined the benchmark class and added a log file

--- benchmark.php
+++ benchmark.php
@@ -2,13 +2,22 @@
 
 require('url-shortener.php');
 
+$reps = 10000;
+
 $time_start = microtime(true);
 
-for ($i = 0; $i < 10000; $i++)
+for ($i = 0; $i < $reps; $i++)
 {
 	$shortener = new Shortener();
 	$var = $shortener->get('94d4');
 	unset($shortener, $var);
 }
 
-echo microtime(true)-$time_start;
+$total_time = microtime(true) - $time_start;
+
+echo 'Repetitions: ' . $reps . '<br />' . PHP_EOL;
+echo 'Total time: ' . ($total_time * 1000) . ' miliseconds<br />' . PHP_EOL;
+echo 'Average time per request: ' . ($total_time * 1000 / $reps) . ' miliseconds';
+
+//log average time per request
+file_put_contents('bench_log', ($total_time * 1000 / $reps) . PHP_EOL, FILE_APPEND);
